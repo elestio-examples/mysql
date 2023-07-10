c3548455dfc46922df93da4758f0ca8a6e7d7e7d
AmsellemJoseph <amsellem.joseph@gmail.com>
2023-07-10 14:49:22 +0300

    fix

--- tests/8.0/build.sh
+++ tests/8.0/build.sh
@@ -1,5 +1,5 @@
 pwd=$(pwd)
 cp "$pwd/8.0/"* "$pwd"
-mv $pwd/Dockerfile.debian $pwd/Dockerfile
+mv $pwd/Dockerfile.oracle $pwd/Dockerfile
 
 docker buildx build . --output type=docker,name=elestio4test/mysql:latest | docker load
