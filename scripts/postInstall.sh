d1ee48d8f097cf028674b64202b22942de48285e
Amit Shukla <53058928+amitshuklabag@users.noreply.github.com>
2023-07-19 19:20:42 +0530

    Update postInstall.sh

--- scripts/postInstall.sh
+++ scripts/postInstall.sh
@@ -1,1 +1,21 @@
-#nothing to do
+#write cli helper
+echo "docker exec -it mysql mysql --host=172.17.0.1 --port=$NETPORT --user=root --password=[APP_PASSWORD]" > mysql-docker-cli.sh;
+chmod +x mysql-docker-cli.sh;
+
+#create backup folder
+mkdir -p /opt/app-backups
+
+#write backup helper
+echo "docker exec mysql /usr/bin/mysqldump --no-tablespaces --set-gtid-purged=OFF --user=root --password=[APP_PASSWORD] --all-databases | gzip > /opt/app-backups/DB_\$(date +%Y-%m-%d-%H.%M.%S).sql.gz" > backupDB.sh;
+chmod +x backupDB.sh;
+
+#write restore from dump helper
+cat > restoreDB-Dump.sh << 'EOF'
+if [ -z "$1" ]
+ then
+echo "You must pass 1 parameter: full path to your SQL backup to restore"
+ else
+gunzip < $1 | docker exec -i mysql /usr/bin/mysql --binary-mode --user=root --password=[APP_PASSWORD]
+fi
+EOF
+chmod +x restoreDB-Dump.sh;
